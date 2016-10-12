3ab7203dc032d36d58eb4ee2025fd734f008dc44
vlong638 <vlong638@163.com>
2016-10-12 18:00:51 +0800

    2016.10.12 18:00

--- 0.Basis/3.Algorithoms/2.13.字符串查找.py
+++ 0.Basis/3.Algorithoms/2.13.字符串查找.py
@@ -1,19 +1,53 @@
+import PrintHelper
+PrintHelper.PrintCode('class Solution:')
+PrintHelper.PrintCode('    def strStr(self, source, target):')
+PrintHelper.PrintCode('        if(len(source)==0 or len(target)==0):')
+PrintHelper.PrintCode('            return -1')
+PrintHelper.PrintCode('        i=0')
+PrintHelper.PrintCode('        while i<len(source):')
+PrintHelper.PrintCode('            j=0')
+PrintHelper.PrintCode('            while j<len(target):')
+PrintHelper.PrintCode('                if source[i+j]==target[j]:')
+PrintHelper.PrintCode('                    print(\"source\",source[i+j])')
+PrintHelper.PrintCode('                    print(\"target\",target[j])')
+PrintHelper.PrintCode('                    j+=1')
+PrintHelper.PrintCode('                    if j==(len(target)):')
+PrintHelper.PrintCode('                        return i')
+PrintHelper.PrintCode('                else:')
+PrintHelper.PrintCode('                    break;')
+PrintHelper.PrintCode('            i+=1')
+PrintHelper.PrintCode('        return -1')
 class Solution:
-# @param s: a list of char
-# @param offset: an integer 
-# @return: nothing
-    def fizzBuzz(self, n):
-        # write you code here
-        result=""
-        ls=list(s)
+    def strStr(self, source, target):
+        if(len(source)==0 or len(target)==0):
+            return -1
         i=0
-        while i<offset:
-            result+=ls.pop(0)
+        while i<len(source):
+            j=0
+            while j<len(target):
+                if source[i+j]==target[j]:
+                    print("source",source[i+j])
+                    print("target",target[j])
+                    j+=1
+                    if j==(len(target)):
+                        return i
+                else:
+                    break;
             i+=1
-        while len(ls)>0:
-            result+=ls.pop()
-        return result
+        return -1
+PrintHelper.PrintCode('s=Solution()')
 s=Solution()
-result= s.rotateString("abcd123456789",2)
+PrintHelper.PrintCode('a=\"abcd123456789\"')
+a="abcd123456789"
+PrintHelper.PrintCode('b=\"123\"')
+b="123"
+PrintHelper.PrintCode('print(\"paramA:\",a)')
+print("paramA:",a)
+PrintHelper.PrintCode('print(\"paramB:\",b)')
+print("paramB:",b)
+PrintHelper.PrintCode('result= s.strStr(a,b)')
+result= s.strStr(a,b)
+PrintHelper.PrintCode('print(type(result))')
 print(type(result))
+PrintHelper.PrintCode('print(result)')
 print(result)
